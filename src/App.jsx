9476268ccfa360a1d38252e87091006beed2a4aa
ghazanfar <ghznfr0078@gmail.com>
2024-12-14 12:49:49 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const increment = () => {
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    setCount(count - 1)
+  }
+  return (
+    <>
+    <button onClick={increment}>Add Value</button>
+    <h3>Counter value: {count} </h3>
+    <button onClick={decrement}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
